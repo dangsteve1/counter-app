8cc06936f57b0ef5517beb564083f2a4285455bd
steve dang <stevedang@Steves-MacBook-Pro.local>
2020-09-04 13:45:03 -0700

    First commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+//stateless functional component
+const NavBar = ({ totalCounters }) => {
+  console.log("NavBar - Rendered");
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {" "}
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
